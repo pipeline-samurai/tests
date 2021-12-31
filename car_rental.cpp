6d27a90db83881c0a1b2550d6c1ace0de47d9368
Alexandru Mihailescu <alecsmihailescu@gmail.com>
2021-12-31 14:23:33 +0200

    adding structures

--- car_rental.cpp
+++ car_rental.cpp
@@ -49,3 +49,32 @@ void password();
 void resetAvail();
 void showCarData();
 void welcome();
+
+struct car
+{
+	char plate_num[10];
+	char brand[20];
+	char model[20];
+	float capacity;
+	char colour[20];
+	float rate_per_hour;
+	float rate_per_half;
+	float rate_per_day;
+	char transmission[6];
+};
+
+struct customer
+{
+	int id;
+	char name[100];
+	char phone[15];
+	char ic[15];
+	char address[1500];
+};
+
+struct pass
+{
+	int ID;
+	char passWord[20];
+	
+};
