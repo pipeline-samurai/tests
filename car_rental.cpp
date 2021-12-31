7f86b8f9819ffd96c8eacb89c7e923b021b43394
Alexandru Mihailescu <alecsmihailescu@gmail.com>
2021-12-31 23:35:34 +0200

    adding coordonate functions

--- car_rental.cpp
+++ car_rental.cpp
@@ -307,3 +307,14 @@ void load_EXIT()
     system("cls");
 }
 
+void GotoXY(int x, int y)
+{
+    HANDLE a;
+    COORD b;
+    fflush(stdout);
+    b.X = x;
+    b.Y = y;
+    a = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleCursorPosition(a,b);
+ }
+
