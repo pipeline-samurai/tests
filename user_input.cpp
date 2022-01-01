fb3fd360ccf9eec6190f7582b07ef8b20fb0abe6
Alexandru <alecsmihailescu@gmail.com>
2022-01-01 23:59:20 +0200

    testing math prob with user input

---
+++ user_input.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std ;
+const double PI = 3.14 ;
+int main()
+{   int r ; 
+    cout << "Enter the radius\n" ; 
+    cin >> r ;
+    cout << "Area is " << PI*r*r ; 
+    return 0 ;
+}
