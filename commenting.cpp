2b20f5d12088a2c3c8590c2f3433e5f999d1102c
Alexandru <alecsmihailescu@gmail.com>
2022-01-01 23:46:12 +0200

    commenting examples

---
+++ commenting.cpp
@@ -0,0 +1,12 @@
+/* Multiline
+Comment
+*/
+#include<iostream>
+using namespace std ;
+int main()
+{
+    cout << "GeeksForGeeks"  ; 
+    //Single Line Comment
+    return 0 ;
+}
+//imagine writing this in vim :)
