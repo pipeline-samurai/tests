74ebcd3579dea92492067a8d10f038d6ad81a364
Alexandru <alecsmihailescu@gmail.com>
2022-01-02 23:59:52 +0200

    adding copy

---
+++ snake.cpp
@@ -0,0 +1,14 @@
+nclude <iostream>
+#include <conio.h>
+#include <fstream>
+#include <string>
+#include <windows.h>
+#pragma comment(lib, "winmm.lib")
+using std::cin;
+using std::cout;
+using std::flush;
+using std::fstream;
+using std::ifstream;
+using std::ios;
+using std::ofstream;
+using std::string;
