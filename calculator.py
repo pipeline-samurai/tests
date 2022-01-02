ff90e7e0c31ba45795ac00c0d099b4005184099a
Alexandru <alecsmihailescu@gmail.com>
2022-01-03 00:58:53 +0200

    calculator python

---
+++ calculator.py
@@ -0,0 +1,26 @@
+print("Welcome to my personal calculator\nPlease input two numbers and an operator:")
+
+user_input1 = input("First number:\n")
+user_input2 = input("Second number:\n")
+user_input3 = input("Operation(add,substract,multiply, divide or raise to power:\n")
+math_add = "add"
+math_substract = "substract"
+math_multiply = "multiply"
+math_divide = "divide"
+math_raise_to_power = "raise to power"
+exit = "quit"
+
+if user_input3 is math_add:
+    print(user_input1 + user_input2)
+if user_input3 is math_substract:
+    print(user_input1 - user_input2)
+if user_input3 is math_multiply:
+    print(user_input1 * user_input2)
+if user_input3 is math_divide:
+    print(user_input1 / user_input2)
+if user_input3 is math_raise_to_power:
+    print(user_input1^user_input2)
+if user_input3 is exit:
+    break()
+
+
