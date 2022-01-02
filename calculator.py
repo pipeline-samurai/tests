3400fdf96da44c147905f2f6a3d336701f741fcb
Alexandru <alecsmihailescu@gmail.com>
2022-01-03 01:23:15 +0200

    refining calculator app

--- calculator.py
+++ calculator.py
@@ -1,7 +1,7 @@
 print("Welcome to my personal calculator\nPlease input two numbers and an operator:")
 
-user_input1 = input("First number:\n")
-user_input2 = input("Second number:\n")
+user_input1 = int(input("First number:\n"))
+user_input2 = int(input("Second number:\n"))
 user_input3 = input("Operation(add,substract,multiply, divide or raise to power:\n")
 math_add = "add"
 math_substract = "substract"
@@ -10,17 +10,17 @@ math_divide = "divide"
 math_raise_to_power = "raise to power"
 exit = "quit"
 
-if user_input3 is math_add:
+if user_input3 == math_add:
     print(user_input1 + user_input2)
-if user_input3 is math_substract:
+if user_input3 == math_substract:
     print(user_input1 - user_input2)
-if user_input3 is math_multiply:
+if user_input3 == math_multiply:
     print(user_input1 * user_input2)
-if user_input3 is math_divide:
+if user_input3 == math_divide:
     print(user_input1 / user_input2)
-if user_input3 is math_raise_to_power:
-    print(user_input1^user_input2)
-if user_input3 is exit:
-    break()
+if user_input3 == math_raise_to_power:
+    print(user_input1**user_input2)
+#if user_input3 is exit:
+    
 
 
