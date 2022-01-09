8d382064772aa38adc56a9429aef7e25d2bab5e0
Alexandru <alecsmihailescu@gmail.com>
2022-01-09 20:43:15 +0200

    python hw 6

---
+++ python_homework_6.py
@@ -0,0 +1,15 @@
+#please correct this
+print(Hi)
+print()
+print"I am mostafa")
+print(3 * 6)
+print"Wonderful " day)
+print("Wonderful " day")
+
+#answer:
+print('Hi')
+print()
+print("I am mostafa")
+print(3 * 6)
+print("Wonderful day")
+print("Wonderful \" day")
