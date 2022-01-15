6dec00ce68a5d66956a7113c7e39fb8a6b222fd0
Alexandru <alecsmihailescu@gmail.com>
2022-01-15 16:17:27 +0200

    python hw2

---
+++ python builds/variables hw2.py
@@ -0,0 +1,15 @@
+#This is the second homework in variables section in the udemy_m4csup
+
+#defining user input
+x1 = input("Enter the first student's name : ")
+y1 = int(input("Enter the first student's ID : "))
+z1 = int(input("Enter the first student's grade: "))
+
+x2 = input("Enter the second student's name : ")
+y2 = int(input("Enter the second student's ID : "))
+z2 = int(input("Enter the second student's grade: "))
+
+#defining output
+
+print("Information for students and their "Math" grades")
+print()
