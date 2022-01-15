074c969af2609045fa4cdc2647ddd9536e690c19
Alexandru <alecsmihailescu@gmail.com>
2022-01-15 19:58:40 +0200

    python hw4

--- python builds/variables hw2.py
+++ python builds/variables hw2.py
@@ -1,19 +1,24 @@
 # This is the second homework in variables section in the udemy_m4csup
 
 # defining user input
-x1 = input("Enter the first student's name : ")
-y1 = int(input("Enter the first student's ID : "))
-z1 = float(input("Enter the first student's grade: "))
+name1 = input("Enter the first student's name : ")
+id1 = (input("Enter the first student's ID : "))
+grade1 = float(input("Enter the first student's grade: "))
 
-x2 = input("Enter the second student's name : ")
-y2 = int(input("Enter the second student's ID : "))
-z2 = float(input("Enter the second student's grade: "))
+name2 = input("Enter the second student's name : ")
+id2 = (input("Enter the second student's ID : "))
+grade2 = float(input("Enter the second student's grade: "))
 
 # defining math variables
-average = (z1+z2)/2
+average = (grade1 + grade2)/2
 # defining output
 
 print('Information for students and their "Math" grades')
-print(x1, "(ID", y1, ")", "got grade:", z1)
-print(x2, "(ID", y2, ")", "got grade:", z2)
+#print(name1, "(ID", id1, ")", "got grade:", grade1)
+msg = name1 + '(ID ' + id1 + ') got grade: ' + str(grade1)
+print(msg)
+#print(name2, "(ID", id2, ")", "got grade:", grade2)
+msg = name2 + '(ID ' + id2 + ') got grade: ' + str(grade2)
+print(msg)
+
 print("Average math grade is: ", average)
