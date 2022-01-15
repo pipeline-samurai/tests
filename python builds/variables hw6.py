774d301c40daf9decc2280d51bd4c39b6d068e9e
Alexandru <alecsmihailescu@gmail.com>
2022-01-15 22:09:48 +0200

    python hw6

---
+++ python builds/variables hw6.py
@@ -0,0 +1,15 @@
+# This is the sixth homework in variables section in the udemy_m4csup
+
+# defining user input
+num1, num2, num3 = map(int, input().split())
+# verifying user input
+print("Before swap: ", num1, num2, num3)
+
+# defining variables and swap logic
+num4 = num1
+num1 = num2
+num2 = num3
+num3 = num4
+
+#defining output
+print("After swap:", num1, num2, num3)
