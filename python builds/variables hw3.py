1888183a5863a1b8a85750bc2f8f78a79d02d96a
Alexandru <alecsmihailescu@gmail.com>
2022-01-15 20:03:51 +0200

    python hw4

--- python builds/variables hw3.py
+++ python builds/variables hw3.py
@@ -1,8 +1,8 @@
 # This is the third homework in variables section in the udemy_m4csup
 
 # defining user input
-n1, n2, n3, n4, n5, n6, n7, n8 = [int(x) for x in input("Enter eight values: ").split()]
-
+#n1, n2, n3, n4, n5, n6, n7, n8 = [int(x) for x in input("Enter eight values: ").split()]
+n1, n2, n3, n4, n5, n6, n7, n8 = map(int, input().split())
 # defining math
 sum_even = n2 + n4 + n6 + n8
 sum_odd = n1 + n3 + n5 + n7
--- python builds/variables hw4.py
+++ python builds/variables hw4.py
@@ -3,5 +3,7 @@
 # defining user input
 str1, str2, str3 = input().split()
 
+combo = str1 + "'" + str2 + '"' + str3
+combo = combo * 10
 # defining output
-print((str1 + str2 + str3) * 10)
+print(combo)
