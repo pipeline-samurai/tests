9341b94379da7053039bad9c64acd722b7c65dae
Alexandru <alecsmihailescu@gmail.com>
2022-01-15 17:29:11 +0200

    python hw3

---
+++ python builds/variables hw3.py
@@ -0,0 +1,11 @@
+# This is the third homework in variables section in the udemy_m4csup
+
+# defining user input
+n1, n2, n3, n4, n5, n6, n7, n8 = [int(x) for x in input("Enter eight values: ").split()]
+
+# defining math
+sum_even = n2 + n4 + n6 + n8
+sum_odd = n1 + n3 + n5 + n7
+
+# defining output
+print(sum_even, sum_odd)
