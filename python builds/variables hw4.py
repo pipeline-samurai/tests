71b3cd08a6c0b1efe660676a824a1ae79e0bf29e
Alexandru <alecsmihailescu@gmail.com>
2022-01-15 19:36:19 +0200

    python hw4

---
+++ python builds/variables hw4.py
@@ -0,0 +1,7 @@
+# This is the third homework in variables section in the udemy_m4csup
+
+# defining user input
+str1, str2, str3 = input().split()
+
+# defining output
+print((str1 + str2 + str3) * 10)
