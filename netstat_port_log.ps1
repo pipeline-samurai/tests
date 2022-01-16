ce30f739ebd183fd5fcdeb3f844a2c67727c7e13
Alexandru <alecsmihailescu@gmail.com>
2022-01-16 23:03:39 +0200

    powershell netstat script for checking open ports

---
+++ netstat_port_log.ps1
@@ -0,0 +1,50 @@
+
+  TCP    0.0.0.0:135            0.0.0.0:0              [7mLISTENING[0m       1288
+  TCP    0.0.0.0:445            0.0.0.0:0              [7mLISTENING[0m       4
+  TCP    0.0.0.0:5040           0.0.0.0:0              [7mLISTENING[0m       5216
+  TCP    0.0.0.0:7680           0.0.0.0:0              [7mLISTENING[0m       16648
+  TCP    0.0.0.0:27036          0.0.0.0:0              [7mLISTENING[0m       12752
+  TCP    0.0.0.0:49664          0.0.0.0:0              [7mLISTENING[0m       788
+  TCP    0.0.0.0:49665          0.0.0.0:0              [7mLISTENING[0m       956
+  TCP    0.0.0.0:49666          0.0.0.0:0              [7mLISTENING[0m       1644
+  TCP    0.0.0.0:49667          0.0.0.0:0              [7mLISTENING[0m       1676
+  TCP    0.0.0.0:49668          0.0.0.0:0              [7mLISTENING[0m       3788
+  TCP    0.0.0.0:64520          0.0.0.0:0              [7mLISTENING[0m       112
+  TCP    10.5.0.2:139           0.0.0.0:0              [7mLISTENING[0m       4
+  TCP    10.5.0.2:18096         0.0.0.0:0              [7mLISTENING[0m       18364
+  TCP    127.0.0.1:6463         0.0.0.0:0              [7mLISTENING[0m       26136
+  TCP    127.0.0.1:6942         0.0.0.0:0              [7mLISTENING[0m       14932
+  TCP    127.0.0.1:9977         0.0.0.0:0              [7mLISTENING[0m       13768
+  TCP    127.0.0.1:9978         0.0.0.0:0              [7mLISTENING[0m       6124
+  TCP    127.0.0.1:18096        0.0.0.0:0              [7mLISTENING[0m       18364
+  TCP    127.0.0.1:27060        0.0.0.0:0              [7mLISTENING[0m       12752
+  TCP    127.0.0.1:49670        0.0.0.0:0              [7mLISTENING[0m       4048
+  TCP    127.0.0.1:49671        0.0.0.0:0              [7mLISTENING[0m       4068
+  TCP    127.0.0.1:53372        0.0.0.0:0              [7mLISTENING[0m       8816
+  TCP    127.0.0.1:53450        0.0.0.0:0              [7mLISTENING[0m       12328
+  TCP    127.0.0.1:54055        0.0.0.0:0              [7mLISTENING[0m       9084
+  TCP    127.0.0.1:63342        0.0.0.0:0              [7mLISTENING[0m       14932
+  TCP    127.0.0.1:65001        0.0.0.0:0              [7mLISTENING[0m       11268
+  TCP    172.22.32.1:139        0.0.0.0:0              [7mLISTENING[0m       4
+  TCP    172.22.32.1:18096      0.0.0.0:0              [7mLISTENING[0m       18364
+  TCP    192.168.100.13:139     0.0.0.0:0              [7mLISTENING[0m       4
+  TCP    192.168.100.13:18096   0.0.0.0:0              [7mLISTENING[0m       18364
+  TCP    [::]:135               [::]:0                 [7mLISTENING[0m       1288
+  TCP    [::]:445               [::]:0                 [7mLISTENING[0m       4
+  TCP    [::]:7680              [::]:0                 [7mLISTENING[0m       16648
+  TCP    [::]:49664             [::]:0                 [7mLISTENING[0m       788
+  TCP    [::]:49665             [::]:0                 [7mLISTENING[0m       956
+  TCP    [::]:49666             [::]:0                 [7mLISTENING[0m       1644
+  TCP    [::]:49667             [::]:0                 [7mLISTENING[0m       1676
+  TCP    [::]:49668             [::]:0                 [7mLISTENING[0m       3788
+  TCP    [::]:64520             [::]:0                 [7mLISTENING[0m       112
+  TCP    [::1]:18096            [::]:0                 [7mLISTENING[0m       18364
+  TCP    [::1]:49669            [::]:0                 [7mLISTENING[0m       4748
+  TCP    [2a02:2f0f:b10f:4d00:9192:72a:c261:6f6e]:18096  [::]:0                 [7mLISTENING[0m       18364
+  TCP    [2a02:2f0f:b10f:4d00:cdb4:74cb:7f4:6e53]:18096  [::]:0                 [7mLISTENING[0m       18364
+  TCP    [fe80::9192:72a:c261:6f6e%14]:18096  [::]:0                 [7mLISTENING[0m       18364
+  TCP    [fe80::fcae:1511:7c6d:7ab0%24]:18096  [::]:0                 [7mLISTENING[0m       18364
+
+
+
+
