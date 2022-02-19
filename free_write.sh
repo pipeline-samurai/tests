eee216b92923117ddfaca1216bcd5a4d9b74010d
Alexandru-Mihailescu <alecsmihailescu@gmail.com>
2022-02-19 20:49:28 +0200

    free_style script update .1

---
+++ free_write.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Do you want to hear a story?"
+read i
+
+if [ $i -eq "yes" ];
+then
+echo "Awesome, I am learning bash scripting in an active manner"
+sleep 1
+echo "So here I am writing some scripts from my head trying to improve"
+sleep 1
+echo "And I would like to showcase my skills by helping you"
+sleep 1
