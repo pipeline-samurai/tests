8d77b443d8d35827eef75f2333b746c5153cc619
Alexandru-Mihailescu <alecsmihailescu@gmail.com>
2022-02-19 20:32:28 +0200

    string operations bash

---
+++ string_operations.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+string1="Alex"
+string2="LifeHub"
+echo "$string1$string2"
+string3=$string1+$string2
+string3+=" is a platform of discovery, teachings and self-reflection "
+echo $string3
