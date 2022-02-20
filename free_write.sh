51cfe6f883eb9b03d7d8c2896143fda72259eb13
Alexandru-Mihailescu <alecsmihailescu@gmail.com>
2022-02-20 22:30:59 +0200

    free_style script update .30

--- free_write.sh
+++ free_write.sh
@@ -11,6 +11,15 @@ echo "So here I am writing some scripts from my head trying to improve"
 sleep 2
 echo "And I would like to showcase my skills by helping you"
 sleep 2
+echo "Let's help you transform seconds into minutes. Pass me a number of seconds:"
+
+read seconds
+minutes = seconds * 60
+
+echo "Your total number of minutes is :" $minutes
+
+
+
 
 else
 echo "I totally understand, have a lovely day!"
