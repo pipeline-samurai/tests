4a9b7272e498d7868a0097d5346e1b918a643d49
Alexandru-Mihailescu <alecsmihailescu@gmail.com>
2022-02-23 14:57:06 +0200

    automation process

--- automation.sh
+++ automation.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 random=$(echo $[ $RANDOM % 3 + 1 ])
+echo
 echo "Hello, I will start pushing $random times to Github"
 sleep 2
 
@@ -8,17 +9,22 @@ valid=true
 count=1
 while [ $valid ]
 do
+echo
 echo $count
 
 pwd=/mnt/d/Github/Projects/tests
-echo "Preparing to write to Github...
+
+echo
+"Preparing to write to Github...
 Setting pwd to $pwd."
 sleep 2
 cd $pwd
+echo
 
 uptime | tee -a counter.sh
 sleep 2
 
+echo
 git add .
 git commit -m "automation process"
 git push
--- counter.sh
+++ counter.sh
@@ -6,3 +6,4 @@ done
 printf "\n"
  14:49:24 up 3 days, 18:52,  0 users,  load average: 0.05, 0.01, 0.00
  14:49:31 up 3 days, 18:52,  0 users,  load average: 0.05, 0.01, 0.00
+ 14:57:04 up 3 days, 18:59,  0 users,  load average: 0.00, 0.00, 0.00
