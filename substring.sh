8cd2571684556d429e8c0542f06a60d5a26826f4
Alexandru-Mihailescu <alecsmihailescu@gmail.com>
2022-03-03 21:07:30 +0200

    automation process

--- substring.sh
+++ substring.sh
@@ -3,3 +3,4 @@ Str="Today I had a wonderful Day"
 subStr=${Str:6:5}
 echo $subStr
  21:07:20 up 12 days,  1:10,  0 users,  load average: 0.00, 0.00, 0.00
+ 21:07:27 up 12 days,  1:10,  0 users,  load average: 0.08, 0.02, 0.01
