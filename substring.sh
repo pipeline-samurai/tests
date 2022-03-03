b261a2f88736dff3d25bde8c27d7422789f1258d
Alexandru-Mihailescu <alecsmihailescu@gmail.com>
2022-03-03 21:07:36 +0200

    automation process

--- substring.sh
+++ substring.sh
@@ -4,3 +4,4 @@ subStr=${Str:6:5}
 echo $subStr
  21:07:20 up 12 days,  1:10,  0 users,  load average: 0.00, 0.00, 0.00
  21:07:27 up 12 days,  1:10,  0 users,  load average: 0.08, 0.02, 0.01
+ 21:07:34 up 12 days,  1:10,  0 users,  load average: 0.07, 0.02, 0.00
