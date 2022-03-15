7cdaab63a4d599d96672a8df4637541db8cfd002
Alexandru-Mihailescu <alecsmihailescu@gmail.com>
2022-03-15 16:15:31 +0200

    automation process

--- string_operations.sh
+++ string_operations.sh
@@ -26,3 +26,4 @@ echo $string3
  16:15:11 up 6 days,  4:34,  0 users,  load average: 0.06, 0.02, 0.00
  16:15:17 up 6 days,  4:34,  0 users,  load average: 0.06, 0.01, 0.00
  16:15:23 up 6 days,  4:34,  0 users,  load average: 0.13, 0.03, 0.01
+ 16:15:29 up 6 days,  4:34,  0 users,  load average: 0.12, 0.03, 0.01
