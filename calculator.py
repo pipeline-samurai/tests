cb69f5c15f6e490faf08eea04ce798228ff29471
Alexandru-Mihailescu <alecsmihailescu@gmail.com>
2022-09-11 01:56:51 +0300

    automation process

--- calculator.py
+++ calculator.py
@@ -652,3 +652,4 @@ if user_input3 == math_raise_to_power:
  20:54:21 up 1 min,  0 users,  load average: 0.07, 0.02, 0.00
  20:54:27 up 1 min,  0 users,  load average: 0.07, 0.02, 0.00
  20:54:33 up 1 min,  0 users,  load average: 0.06, 0.02, 0.00
+ 01:56:48 up 2 days,  5:03,  0 users,  load average: 0.00, 0.00, 0.00
